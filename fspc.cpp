86bfd022f89caf95c75efe42fcb59dafec7aae5c
Eclipse <0-Eclipse-0@users.noreply.github.com>
2021-10-03 16:58:34 -0400

    Added support for file outputting
    Made it so that input data can be logged into a csv file

--- fspc.cpp
+++ fspc.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <iomanip>
 
 using namespace std;
@@ -34,22 +35,30 @@ void bad_inp(int type){
 class Product
 {
   public:
-    void EnterInfo();
+    void EnterInfo(bool out_2_file);
     int DispPortSize()
     {
       return portion_size;
     }
     float GetPrice();
     int GetNumPort();
+    void OutFileText(char filename[50]);
   private:
     int units;
     int unit_size;
     float case_cost;
     int portion_size;
+    char prod_name[50];
 };
 
-void Product::EnterInfo()
+void Product::EnterInfo(bool out_2_file)
 {
+  if(out_2_file){
+    cin.ignore();
+    cout<<pn<<"Enter the product name: ";
+    cin.getline(prod_name, 49);
+  }
+
   cout<<pn<<"Enter the number of units per case (i.e. bags): ";
   while((!(cin >> units)) | (units <= 0)){
     bad_inp(1);
@@ -93,6 +102,15 @@ int Product::GetNumPort()
   return (units*unit_size)/portion_size;
 }
 
+void Product::OutFileText(char filename[50]) {
+  ofstream output_file;
+  output_file.open(filename, ios::app);
+  output_file<<prod_name<<", "<<units<<", $"<<case_cost;
+  output_file<<", "<<unit_size<<"oz, "<<portion_size;
+  output_file<<"oz, "<<GetNumPort()<<", $"<<setprecision(2);
+  output_file<<fixed<<GetPrice()<<endl;
+}
+
 void cls()
 {
   cout<<"\033[2J\033[1;1H";
@@ -103,23 +121,45 @@ void welcome_msg()
   cout<<"[Food Service Price Calculator]"<<endl;
 }
 
-void startup(){
+void startup()
+{
   cls();
   welcome_msg();
 }
 
-int main(){
+void get_info(bool out_file)
+{
   Product NewProd;
   bool continue_prog = 1;
   char usr_chc;
-  startup();
+  char filename[50];
 
-  while(continue_prog){
-    NewProd.EnterInfo();
+  if (out_file) {
+    ofstream output_file;
+    cout<<pn<<"Enter file name (Max: 50 characters): ";
+    cin>>filename;
+    strcat(filename, ".csv");
+    output_file.open(filename, ios::out | ios::trunc);
+    output_file<<"Product Name, Case Size, Case Cost, Unit Size, ";
+    output_file<<"Portion Size, Estimated Portions, Estimated Price Per"<<endl;
+  }
+
+while(continue_prog){
+    if(out_file){
+      NewProd.EnterInfo(1);
+    }else{
+      NewProd.EnterInfo(0);
+    }
+
     cout<<pn<<"For a portion size of "<<NewProd.DispPortSize();
     cout<<"oz you should be charging ~$"<<setprecision(2)<<fixed<<NewProd.GetPrice();
     cout<<" per sale \nwith a total of ~"<<NewProd.GetNumPort();
     cout<<" available portions per case!"<<endl;
+
+    if(out_file){
+      NewProd.OutFileText(filename);
+      cout<<pn<<"Information outputted to <"<<filename<<".csv>"<<endl;
+    }
 
     cout<<pn<<"Would you like to calculate the price for another product? (Y/n): ";
     cin>>usr_chc;
@@ -135,6 +175,24 @@ int main(){
         bad_inp(2);
         exit(3);
     };
+  }
+}
+
+int main(){
+  bool valid_inp;
+  char usr_chc;
+
+  startup();
+
+  cout<<pn<<"Would you like to output your product information to a file? (Y/n): ";
+  cin >> usr_chc;
+
+  if (usr_chc == 'Y' | usr_chc == 'y') {
+    get_info(1);
+  } else if (usr_chc == 'N' | usr_chc == 'n') {
+    get_info(0);
+  } else {
+    bad_inp(2);
   }
 
   return 0;
